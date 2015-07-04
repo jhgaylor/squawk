20da9efd3938f17925a9195367c52651dec4cf68
Jake Gaylor <jhgaylor@gmail.com>
2015-07-03 22:52:30 -0600

    build express shell

--- api-server/src/index.js
+++ api-server/src/index.js
@@ -1,5 +1,15 @@
+
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
+var ClientRouter = require('./routers/clients');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use('/clients', ClientRouter);
 
 app.get('/', function(req, res) {
   res.send('Hello World!!!!!!');
@@ -7,10 +17,7 @@ app.get('/', function(req, res) {
 
 var port = process.env.PORT || 3000;
 var server = app.listen(port, function() {
-
   var host = server.address().address;
   var port = server.address().port;
-
   console.log('Example app listening at http://%s:%s', host, port);
-
 });
