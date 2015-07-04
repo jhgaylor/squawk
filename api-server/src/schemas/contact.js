1b4579a062ba3217512b4dd59914cb2fe212fb97
Jake Gaylor <jhgaylor@gmail.com>
2015-07-04 03:19:20 -0600

    finalize contact schema 0.0.1

--- api-server/src/schemas/contact.js
+++ api-server/src/schemas/contact.js
@@ -3,18 +3,23 @@
     var ContactSchema = mongoose.Schema({
       // user model id. represents whose address book contains
       // this projection of the contact.
-      ownerId: mongoose.Schema.Types.ObjectId,
-      name: {
-        type: String
+      ownerId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
       },
       number: {
         type: String,
         required: true
       },
+      name: {
+        type: String,
+        default: ''
+      },
       type: {
         type: String,
         lowercase: true,
-        enum: ['cell', 'home', 'work', 'twilio']
+        enum: ['cell', 'home', 'work', 'twilio', 'unknown'],
+        default: 'unknown'
       }
     });
     return ContactSchema;
