a5f1ad1e438d6f2ca80a804396ef12baef8d1724
Jake Gaylor <jhgaylor@gmail.com>
2015-07-04 05:02:37 -0600

    add test for /users

--- api-server/spec/main.js
+++ api-server/spec/main.js
@@ -5,36 +5,48 @@ var Models = SquawkServer.Models;
 var SquawkServerApp = request.agent(SquawkServer.app);
 
 describe('Squawk Api Server', function() {
-  beforeAll(function(done) {
-    Models.User.create({
-      'profile.firstName': 'TEST',
-      'twilio.number': '+16626940191'
-    }, function(err, res) {
-      done();
+  describe('Users endpoint', function() {
+    beforeAll(function(done) {
+      Models.User.create({
+        'profile.firstName': 'TEST',
+        'twilio.number': '+16626940191'
+      }, function(err, res) {
+        done();
+      });
     });
-  });
 
-  afterAll(function(done) {
-    Models.User.remove({
-      'profile.firstName': 'TEST',
-      'twilio.number': '+16626940191'
-    }, function(err, res) {
-      done();
+    afterAll(function(done) {
+      Models.User.remove({
+        'profile.firstName': 'TEST',
+        'twilio.number': '+16626940191'
+      }, function(err, res) {
+        done();
+      });
     });
-  });
 
-  it('allows looking up a single user', function(done) {
-    Models.User.findOne({'profile.firstName': 'TEST'}, function(err, user) {
-      expect(user && user.twilio && user.twilio.number).toBeDefined();
-      var userNumber = user.twilio.number;
+    it('gives helpful message when requesting all users', function(done) {
       SquawkServerApp
-        .get('/users/' + userNumber)
+        .get('/users')
         .end(function(err, res) {
           expect(err).toBe(null);
-          expect(res.body.data).toBeDefined;
-          expect(res.body.data.twilio.number).toEqual(userNumber)
+          expect(res.body.message).toBeDefined();
           done();
         });
+    });
+
+    it('allows looking up a single user', function(done) {
+      Models.User.findOne({'profile.firstName': 'TEST'}, function(err, user) {
+        expect(user && user.twilio && user.twilio.number).toBeDefined();
+        var userNumber = user.twilio.number;
+        SquawkServerApp
+          .get('/users/' + userNumber)
+          .end(function(err, res) {
+            expect(err).toBe(null);
+            expect(res.body.data).toBeDefined;
+            expect(res.body.data.twilio.number).toEqual(userNumber)
+            done();
+          });
+      });
     });
   });
 });
