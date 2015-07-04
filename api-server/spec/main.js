ab67238ac10d487a3bc3d34eed6b3002cf48b6c6
Jake Gaylor <jhgaylor@gmail.com>
2015-07-04 04:51:20 -0600

    built first tests using supertest to fake the server

--- api-server/spec/main.js
+++ api-server/spec/main.js
@@ -1,9 +1,44 @@
-describe('A suite', function() {
-  it('contains spec with an expectation', function() {
-    expect(true).toBe(true);
+var request = require('supertest');
+var SquawkServer = require('../src/index');
+var mongoose = SquawkServer.mongoose;
+var Models = SquawkServer.Models;
+var SquawkServerApp = request.agent(SquawkServer.app);
+
+describe('Squawk Api Server', function() {
+  beforeAll(function(done) {
+    Models.User.create({
+      'profile.firstName': 'TEST',
+      'twilio.number': '+16626940191'
+    }, function(err, res) {
+      done();
+    });
   });
 
-  // it('contains spec with an expectation that will fail', function() {
-  //   expect(true).toBe(false);
-  // });
+  afterAll(function(done) {
+    Models.User.remove({
+      'profile.firstName': 'TEST',
+      'twilio.number': '+16626940191'
+    }, function(err, res) {
+      done();
+    });
+  });
+
+  it('allows looking up a single user', function(done) {
+    Models.User.findOne({'profile.firstName': 'TEST'}, function(err, user) {
+      expect(user).toBeDefined();
+      expect(user.twilio).toBeDefined();
+      expect(user.twilio.number).toBeDefined();
+      var userNumber = user.twilio.number;
+      SquawkServerApp
+        .get('/users/' + userNumber)
+        .end(function(err, res) {
+          expect(err).toBe(null);
+          expect(res.body.data).toBeDefined;
+          var data = res.body.data;
+          expect(data).toBeDefined()
+          expect(data.twilio.number).toEqual(userNumber)
+          done();
+        });
+    });
+  });
 });
