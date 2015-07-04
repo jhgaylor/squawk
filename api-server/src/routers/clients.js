94fe1fcac44bd473baba62d873c18055d5ff92c4
Jake Gaylor <jhgaylor@gmail.com>
2015-07-03 22:52:25 -0600

    stub a router

---
+++ api-server/src/routers/clients.js
@@ -0,0 +1,10 @@
+var router = require('express').Router();
+// var Q = require('q');
+// var moment = require('moment');
+// var MongoClient = require('mongodb').MongoClient
+// var _ = require('underscore');
+
+router.route('/')
+  .get(function(err, response) {});
+
+module.exports = router;
