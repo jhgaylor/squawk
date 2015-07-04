dbe952d445bc1057163a500ad728f21f4a847933
Jake Gaylor <jhgaylor@gmail.com>
2015-07-04 04:00:53 -0600

    return a single user

--- api-server/src/routers/users.js
+++ api-server/src/routers/users.js
@@ -6,10 +6,43 @@
     var router = require('express').Router();
     router.route('/')
       .get(nothing);
+
+    router.route('/:number')
+      .get(getUserByNumber);
+
     function nothing (req, res) {
       res.json({message: 'There is nothing to see here.'});
     }
+
+    function getUserByNumber (req, res) {
+      var number = req.params.number;
+      if (!number || number.length < 10) {
+        res.status(400).json({
+          error: 'Please specify a phone number /users/+15551234567'
+        });
+        return;
+      }
+      if (number.length === 10) {
+        number = '+1' + number;
+      } else if (number.length === 11) {
+        number = '+' + number;
+      }
+
+      Models.User.findOne({'twilio.number': number}, function(err, user) {
+        if (err) {
+          res.sendStatus(500);
+          return;
+        }
+        if (user) {
+          res.json({data: user});
+          return;
+        }
+        res.status(404).json({
+          error: 'No user matching the specified number was found. Please register.'
+        });
       });
+    }
+
     return router;
   }
 
