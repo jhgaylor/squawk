6ee65f7bd40a7437f285babeaa50222f2f223fae
Jake Gaylor <jhgaylor@gmail.com>
2015-07-05 00:42:15 -0600

    modify utils

--- api-server/src/util.js
+++ api-server/src/util.js
@@ -1,17 +1,53 @@
 (function() {
+  var Q = require('q');
+  // TODO: implement
+
+  function processTwilioResponse (message, response) {
+    var def = Q.defer();
+    // TODO: process the response and return a combined
+    //       object.
+    message.twilio = response;
+    def.resolve(message);
+    return def.promise;
+  }
+
+  function sendMessageViaTwilio (twilioClient, message) {
+    var def = Q.defer();
+    // TODO: add MMS support
+    twilioClient.sendMessage({
+      to: message.to,
+      from: message.from,
+      body: message.body
+    }, function(err, response) {
+      if (err) {
+        def.reject(err);
+        return;
+      }
+      processTwilioResponse(message, response)
+        .then(function(finalMessage) {
+          def.resolve(finalMessage);
+        }, function(err) {
+          def.reject(err);
+        });
+    });
+    return def.promise;
+  }
+
   function makeTwilioNumber (number) {
     if (number.length < 10) {
       return false;
     }
     if (number.length === 10) {
-      number = '+1' + number;
+      return '+1' + number;
     } else if (number.length === 11) {
-      number = '+' + number;
+      return '+' + number;
+    } else {
+      return number;
     }
-    return number;
   }
 
   module.exports = {
-    makeTwilioNumber: makeTwilioNumber
+    makeTwilioNumber: makeTwilioNumber,
+    twilioSend: sendMessageViaTwilio
   };
 })();
