fcd03a258a5b7c2fbce10a952ed202db4a9355e3
Jake Gaylor <jhgaylor@gmail.com>
2015-07-05 14:56:49 -0600

    cleanup docs

--- desktop-client/src/main.js
+++ desktop-client/src/main.js
@@ -11,9 +11,6 @@ var mainWindow = null;
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
   app.quit();
-  // if (process.platform != 'darwin') {
-  //   app.quit();
-  // }
 });
 
 // This method will be called when Electron has done everything
@@ -22,14 +19,12 @@ app.on('ready', function() {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600});
 
-  // and load the index.html of the app.
+  // and load the angular app.
   mainWindow.loadUrl('file://' + __dirname + '/client/index.html');
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
+    // Dereference the window object
     mainWindow = null;
   });
 });
