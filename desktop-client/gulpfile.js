9bed8ee1f3ec1709fc6de2d604611606cb70cdae
Jake Gaylor <jhgaylor@gmail.com>
2015-07-06 21:18:30 -0600

    refactor to use blobs hash

--- desktop-client/gulpfile.js
+++ desktop-client/gulpfile.js
@@ -1,14 +1,35 @@
 var gulp = require('gulp');
 var jscs = require('gulp-jscs');
 var jasmine = require('gulp-jasmine');
+var watch = require('gulp-watch')
+var sass = require('gulp-sass');
+var inject = require('gulp-inject');
+var batch = require('gulp-batch');
 var wiredep = require('wiredep').stream;
 
 var appSourceBlob = 'src/**/*.js';
+var blobs = {
+  app: './src/client',
+  html: {
+    main: './src/client/index.html'
+  },
+  css: {
+    all: './src/client/css/**/*.css'
+  },
+  scss: {
+    buildTo: './src/client/css',
+    all: 'src/client/scss/**/*.scss',
+    main: '/src/client/scss/main.scss'
+  },
+  js: {
+    all: './src/client/**/*.js'
+  }
+};
 
 gulp.task('bower', function() {
-  gulp.src('./src/client/index.html')
+  gulp.src(blobs.html.main)
     .pipe(wiredep())
-    .pipe(gulp.dest('./src/client'));
+    .pipe(gulp.dest(blobs.app));
 });
 
 gulp.task('default', ['lint'], function defaultTask () {
