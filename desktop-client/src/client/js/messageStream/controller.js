4f48fd94219498fef425691b6307f064df0bac4b
Jake Gaylor <jhgaylor@gmail.com>
2015-07-06 23:36:28 -0600

    give useful feedback on empty message stream

--- desktop-client/src/client/js/messageStream/controller.js
+++ desktop-client/src/client/js/messageStream/controller.js
@@ -1,9 +1,13 @@
 var Squawk = angular.module('squawk');
-Squawk.controller('MessageStreamController', ['$scope', 'messageStreamService',
-  function($scope, messageStreamService) {
+Squawk.controller('MessageStreamController', ['$scope', 'messageStreamService', 'activeContactService',
+  function($scope, messageStreamService, activeContactService) {
     $scope.$on('messageStream:updated', function(event, messages) {
       $scope.messages = messages;
     });
+    $scope.$on('activeContactService:updated', function(event, activeContact) {
+      $scope.activeContact = activeContact;
+    });
     $scope.messages = messageStreamService.get();
+    $scope.activeContact = activeContactService.get();
   }
 ]);
